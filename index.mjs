271613138282dd27d130fc1855005739f201f313
James Ho <hoangphuc.ho@frosmo.com>
2022-05-28 02:18:18 +0300

    first commit

---
+++ index.mjs
@@ -0,0 +1,9 @@
+const { randomUUID } = await import('node:crypto');
+
+const logAfter5s = () => {
+  const time = new Date().toISOString();
+  console.log(`${time}: ${randomUUID()}`);
+  setTimeout(logAfter5s, 5000);
+};
+
+logAfter5s();
